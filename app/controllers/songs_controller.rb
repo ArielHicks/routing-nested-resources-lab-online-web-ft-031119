019bb80ee3dc9b35d34a78126e47e913baf84d0c
Ariel Hicks <hicks.ariel@gmail.com>
2019-05-29 16:31:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,7 +3,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
       if artist.nil?
-        redirect_to artist_path
+        redirect_to artist_path, alert: "Artist Path"
     @songs = Song.all
   end
 
