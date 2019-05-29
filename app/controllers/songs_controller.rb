24cdde8a87d0a3ea1fe8edd6c6ceee4eb0e45935
Ariel Hicks <hicks.ariel@gmail.com>
2019-05-29 16:36:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,6 +19,8 @@ end
       @song = artist.songs.find_by(id: params[:id])
       if @song.nil?
         redirect_to artist_songs_path(artist), alert: "Song not found"
+      end
+    else
     @song = Song.find(params[:id])
   end
 
