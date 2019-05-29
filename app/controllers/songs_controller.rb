3974a77d566c8cf1d50811f5b3e24aa962cfd45a
Ariel Hicks <hicks.ariel@gmail.com>
2019-05-29 16:31:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,6 +2,8 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artist
     @songs = Song.all
   end
 
