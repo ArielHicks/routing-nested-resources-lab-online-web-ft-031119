3490df21c64f91329192d9109243a4abe956803e
Ariel Hicks <hicks.ariel@gmail.com>
2019-05-29 16:36:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,7 +18,7 @@ end
       artist = Artist.find_by(id: params[:artist_id])
       @song = artist.songs.find_by(id: params[:id])
       if @song.nil?
-        redirect_to artist_songs_path(artist)
+        redirect_to artist_songs_path(artist), alert: "Song not found"
     @song = Song.find(params[:id])
   end
 
